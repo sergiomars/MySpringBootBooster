7d9a7857a69c87a655118dc6d84813b62c4a780f
sergio <sergio.marsella@gmail.com>
2022-02-06 16:26:57 +0100

    introduce basic application

---
+++ MyApplication/src/main/java/com/example/springboot/api/PetsController.java
@@ -0,0 +1,15 @@
+package com.example.springboot.api;
+
+import org.myApi.api.PetsApi;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PetsController implements PetsApi {
+
+    @Override
+    public ResponseEntity<Void> createPets(){
+
+        return ResponseEntity.ok().build();
+    }
+}
