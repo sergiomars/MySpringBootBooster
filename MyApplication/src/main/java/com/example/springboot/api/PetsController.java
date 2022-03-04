771940ba2fb899f88af94101d9e53589fdf0e267
sergio <sergio.marsella@gmail.com>
2022-03-04 14:53:47 +0100

    move dependency versions to main project

--- MyApplication/src/main/java/com/example/springboot/api/PetsController.java
+++ MyApplication/src/main/java/com/example/springboot/api/PetsController.java
@@ -1,11 +1,19 @@
 package com.example.springboot.api;
 
+import io.swagger.annotations.ApiParam;
 import org.myApi.api.PetsApi;
+import org.myApi.model.Pet;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PetsController implements PetsApi {
+
+    @Override
+    public ResponseEntity<Pet> showPetById(@ApiParam(value = "The id of the pet to retrieve", required = true) @PathVariable("petId") String petId) {
+        return ResponseEntity.ok(new Pet());
+    }
 
     @Override
     public ResponseEntity<Void> createPets() {
